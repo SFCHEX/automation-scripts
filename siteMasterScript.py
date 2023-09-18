ab35c7de362dafded3a6221f5c83965cd373844a
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-09-18 14:36:27 +0300

    Update siteMasterScript.py

--- siteMasterScript.py
+++ siteMasterScript.py
@@ -1,23 +1,37 @@
 
 def updateSiteData(date_str, pivot_table_ava, pivot_table_pad, pivot_table_par, xlsb_file_path):
     with pyxlsb.open_workbook(xlsb_file_path) as wb:
-        for sheet_name in wb.sheets:
-            with wb.get_sheet(sheet_name) as sheet:
-                data = []
-                for row in sheet.rows():
-                    data_row = [item.v for item in row]
-                    data.append(data_row)
-                section_data = pd.DataFrame(data[2:], columns=data[1])
+        with pyxlsb.get_sheet(wb, sheet_name='AVA') as sheet_ava:
+            data_ava = []
+            for row in sheet_ava.rows():
+                data_ava.append([item.v for item in row])
 
-            with pyxlsb.open_workbook(xlsb_file_path, use_memory_mmap=True) as wb:
-                writer = pd.ExcelWriter(xlsb_file_path, engine='pyxlsb')
-                writer.book = wb
+        with pyxlsb.get_sheet(wb, sheet_name='PAD') as sheet_pad:
+            data_pad = []
+            for row in sheet_pad.rows():
+                data_pad.append([item.v for item in row])
 
-                for row1_value, pivot_table in zip(['AVA', 'PAD', 'PAR'], [pivot_table_ava, pivot_table_pad, pivot_table_par]):
-                    if row1_value == sheet_name:
-                        date_col_idx = section_data.columns.get_loc(date_str)
+        with pyxlsb.get_sheet(wb, sheet_name='PAR') as sheet_par:
+            data_par = []
+            for row in sheet_par.rows():
+                data_par.append([item.v for item in row])
 
-                        section_data[date_str] = pivot_table.set_index('SITE ID')['Data']
-                        section_data.to_xls(writer, sheet_name, index=False, header=False, startrow=2)
+    section_data_ava = pd.DataFrame(data_ava[2:], columns=data_ava[1])
+    section_data_pad = pd.DataFrame(data_pad[2:], columns=data_pad[1])
+    section_data_par = pd.DataFrame(data_par[2:], columns=data_par[1])
 
-                writer.save()
+    # Update the Data column for the specified date in each section
+    section_data_ava[date_str] = pivot_table_ava.set_index('SITE ID')['Data']
+    section_data_pad[date_str] = pivot_table_pad.set_index('SITE ID')['Data']
+    section_data_par[date_str] = pivot_table_par.set_index('SITE ID')['Data']
+
+    # Save the updated data back to the XLSB file
+    with pyxlsb.open_workbook(xlsb_file_path, 'w') as wb:
+        with wb.get_sheet(sheet_name='AVA') as sheet_ava:
+            sheet_ava.writer.writerows([section_data_ava.columns] + section_data_ava.values.tolist())
+
+        with wb.get_sheet(sheet_name='PAD') as sheet_pad:
+            sheet_pad.writer.writerows([section_data_pad.columns] + section_data_pad.values.tolist())
+
+        with wb.get_sheet(sheet_name='PAR') as sheet_par:
+            sheet_par.writer.writerows([section_data_par.columns] + section_data_par.values.tolist())
