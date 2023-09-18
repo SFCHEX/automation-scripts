e34e113eb8480df3590e8855cb077b574c3d496e
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-09-18 14:30:56 +0300

    Update siteMasterScript.py

--- siteMasterScript.py
+++ siteMasterScript.py
@@ -9,18 +9,15 @@ def updateSiteData(date_str, pivot_table_ava, pivot_table_pad, pivot_table_par, 
                     data.append(data_row)
                 section_data = pd.DataFrame(data[2:], columns=data[1])
 
-            writer = pd.ExcelWriter(xlsb_file_path, engine='openpyxl')
-            writer.book = load_workbook(writer.path)
-            
-            for row1_value, pivot_table in zip(['AVA', 'PAD', 'PAR'], [pivot_table_ava, pivot_table_pad, pivot_table_par]):
-                if row1_value == sheet_name:
-                    date_col_idx = section_data.columns.get_loc(date_str)
+            with pyxlsb.open_workbook(xlsb_file_path, use_memory_mmap=True) as wb:
+                writer = pd.ExcelWriter(xlsb_file_path, engine='pyxlsb')
+                writer.book = wb
 
-                    writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
-                    writer.sheets[sheet_name] = writer.book.create_sheet(title=sheet_name)
+                for row1_value, pivot_table in zip(['AVA', 'PAD', 'PAR'], [pivot_table_ava, pivot_table_pad, pivot_table_par]):
+                    if row1_value == sheet_name:
+                        date_col_idx = section_data.columns.get_loc(date_str)
 
-                    section_data[date_str] = pivot_table.set_index('SITE ID')['Data']
-                    section_data.to_excel(writer, sheet_name, startcol=date_col_idx, index=False, header=False)
+                        section_data[date_str] = pivot_table.set_index('SITE ID')['Data']
+                        section_data.to_xls(writer, sheet_name, index=False, header=False, startrow=2)
 
-            writer.save()
-            writer.close()
+                writer.save()
