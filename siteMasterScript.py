8450a06d7d28775fda413eac47b896a7193bf777
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-09-19 17:40:11 +0300

    Update siteMasterScript.py

--- siteMasterScript.py
+++ siteMasterScript.py
@@ -19,9 +19,11 @@ def updateNetworkAva(avaSheetName):
         avaSheetMaster=avaWBMaster['Sheet2']
         avaWBSheet=openpyxl.load_workbook(avaSheetName)
         avaSheet=avaWBSheet['Sheet1']
+        print("opened AVA master sheet")
         for row in avaSheet.iter_rows(min_row=2, values_only=True):
                 avaSheetMaster.append(row)
         avaWBMaster.save(avaSheetMasterName)
+        print("Added values to master sheet")
     except Exception as e:
         print(e)
 
@@ -33,9 +35,11 @@ def updatePowerAlarm(powerSheetName):
         powerSheetMaster=powerWBMaster['Sheet1']
         powerWBSheet=openpyxl.load_workbook(powerSheetName)
         powerSheet=powerWBSheet['Sheet1']
+        print("opened power master sheet")
         for row in powerSheet.iter_rows(min_row=2, values_only=True):
                 powerSheetMaster.append(row)
         powerWBMaster.save(powerSheetMasterName)
+        print("added values to master sheet")
     except Exception as e:
         print(e)
 
@@ -43,18 +47,20 @@ def updatePowerAlarm(powerSheetName):
 
 def getAVA(avaSheet):
     avaSheet=avaSheet.loc[avaSheet["TECH"]!="4G"]
+    print(avaSheet)
     AVA = pd.pivot_table(avaSheet, values=r'AVA (%)', index='SITE',aggfunc='mean')
-    blank_columns = pd.DataFrame('', index=range(len(avaSheet)), columns=[''] * 3)  # Insert 3 blank columns
-    avaSheet=pd.concat([avaSheet,blank_columns,avaSheet],axis=1)
+#   blank_columns = pd.DataFrame('', index=range(len(avaSheet)), columns=[''] * 3)  # Insert 3 blank columns
+#   avaSheet=pd.concat([avaSheet,blank_columns,avaSheet],axis=1)
     return AVA
 
 
 def getPADPAR(powerSheet):
     powerSheet=powerSheet.loc[powerSheet['ALARM']=="Yes"]
+    print(powerSheet)
     PAD = pd.pivot_table(powerSheet, values='MTTR.1', index='Site Code', aggfunc='sum')
     PAR = pd.pivot_table(powerSheet, values='MTTR.1', index='Site Code', aggfunc='count')
-    blank_columns = pd.DataFrame('', index=range(len(powerSheet)), columns=[''] * 3)  # Insert 3 blank columns
-    powerSheet=pd.concat([powerSheet,blank_columns,PAD,blank_columns,PAR],axis=1)
+#   blank_columns = pd.DataFrame('', index=range(len(powerSheet)), columns=[''] * 3)  # Insert 3 blank columns
+#   powerSheet=pd.concat([powerSheet,blank_columns,PAD,blank_columns,PAR],axis=1)
  
     return PAD,PAR
 
@@ -77,43 +83,35 @@ def updateSiteData(AVA,PAD,PAR,date,output):
             print(f"{str(cell.value)==str(date)}")
             if str(cell.value)==str(date):
                 count+=1
+                if count==0:
+                    func="AVA (%)"
+                else:
+                    func="MTTR.1"
                 print("Modifying sheet now")
                 for row_index in range(3, sheet.max_row+1):
                     cell2=sheet[f'A{row_index}']
                     print(f"looping through column {cell.col_idx}")
                     cell2_value=cell2.value
+                    changedCell=sheet.cell(column=cell.col_idx, row=row_index)
                     if cell2_value in data[count]:
                         print("changing values")
-                        changedCell=sheet.cell(column=cell.col_idx, row=row_index)
-                        if count==0:
-                            func="AVA (%)"
-                        else:
-                            func="MTTR.1"
                         changedCell.value=data[count][cell2_value][func]
-                 
-
-    
-
-
+                    else:
+                        changedCell.value=0
     wb.save(output)
-
-
-
-    pass
 
 
 def main():
     parser = argparse.ArgumentParser(description="DataFornetAv")
     parser.add_argument("-a", "--avaSheetName", help="availability Update")
-
     parser.add_argument("-p", "--powerSheetName", help="power update")
     parser.add_argument("-o", "--outputSheetName", help="Update Sheet")
     args =parser.parse_args()
 
     avaSheet,powerSheet=loadSheets(args.avaSheetName,args.powerSheetName)
     print("Loaded Sheets")
-#   updateNetworkAva(args.avaSheetName)
-#   updatePowerAlarm(args.powerSheetName)
+    #updateNetworkAva(args.avaSheetName)
+    #updatePowerAlarm(args.powerSheetName)
     AVA=getAVA(avaSheet)
     print("received AVA")
     PAD,PAR=getPADPAR(powerSheet)
