230ccbc98a5654a844c62cf2847a017a24dce804
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-09-25 18:44:27 +0300

    Update masterGUI.py

--- masterGUI.py
+++ masterGUI.py
@@ -17,30 +17,11 @@ def run_script(script_name):
         try:
             def run_subprocess():
                 process = subprocess.Popen(["python", script_dir + script_name], cwd=directory_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                while True:
-                    output = process.stdout.readline()
-                    if output == '' and process.poll() is not None:
-                        break
-                    if output:
-                        print(output.strip())  # Print to console
-                        output_queue.put(output)  # Put output into the queue for UI update
-
             threading.Thread(target=run_subprocess).start()
         except FileNotFoundError:
             print(f"Script {script_name} not found.")
     else:
         print("Please select a directory first.")
-
-def update_output():
-    while True:
-        output = output_queue.get()
-        if output is None:
-            break
-        output_text_widget.config(state="normal")
-        output_text_widget.insert(tk.END, output)
-        output_text_widget.config(state="disabled")
-        output_text_widget.see(tk.END)  # Scroll to the end
-        root.update_idletasks()  # Update the UI
 
 root = tk.Tk()
 root.title("Script Runner")
@@ -69,22 +50,8 @@ for script_name in script_names:
     if col > 1:
         col = 0
         row += 1
-
-output_frame = ttk.Frame(root)
-output_frame.grid(row=row + 1, columnspan=2, padx=10, pady=10, sticky="nsew")
-output_frame.columnconfigure(0, weight=1)
-output_frame.rowconfigure(0, weight=0)  # Set the weight to 0 to make it smaller
-
+        
 directory_var = tk.StringVar()
 
-output_text_widget = tk.Text(output_frame, wrap=tk.WORD, state="disabled", font=("Helvetica", 12))
-output_text_widget.grid(row=0, column=0, sticky="nsew")
-
-output_queue = queue.Queue()  # Create a queue for output
-
-# Start the output update thread
-output_thread = threading.Thread(target=update_output)
-output_thread.daemon = True  # Allow the thread to exit when the main program exits
-output_thread.start()
 
 root.mainloop()
